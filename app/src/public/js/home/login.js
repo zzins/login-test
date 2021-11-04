458d936846981aa3566684e02a04890524d1dfc4
leejinhui <zzin@leejinhuiui-MacBookAir.local>
2021-11-04 18:43:06 +0900

    login

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,13 @@
 "use strict";
 
-console.log("test11111");
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const loginBtn = document.querySelector("#btnLogin");
+
+loginBtn.addEventListener("click", () => {
+    const req = {
+        id: id.value,
+        pw: password.value,
+    };
+    console.log(req);
+});
