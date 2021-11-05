2486f4b94b4c63981f72c68c7c97f3be27311cfa
leejinhui <zzin@leejinhuiui-MacBookAir.local>
2021-11-05 13:13:13 +0900

    body parser

--- app/app.js
+++ app/app.js
@@ -7,9 +7,9 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-app.use("/", home);
 app.use(express.static(`${__dirname}/src/public`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/", home);
 
 module.exports = app;
